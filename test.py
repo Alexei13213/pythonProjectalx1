52bf10ba2d76b4dd3f956ba738d53f796d8e0bf9
mhaitov <81020414+mhaitov@users.noreply.github.com>
2022-01-18 15:11:34 +0200

    add file

---
+++ test.py
@@ -0,0 +1,13 @@
+# This is a comment
+name = input('Enter name: ')
+Lastname = input('Enter last name: ')
+age = input('Enter age' )
+print(type(age))
+
+print('Hello', name, Lastname + "! You are", age, '.')
+print('Hello' + )
+
+
+print('Hello', name, Lastname +"!You are", age,'years old!')
+print('Hello ' + name + ' ' + Lastname + '! You are ' + str(age) + ' year old!')
+print("Hello", True, None, 1000, 12.123)
